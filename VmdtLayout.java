5a57df4bfc18560d019d2c99cb1c222006ecf84d
medmeg <mehdi.megzari@mysportconnect.net>
2013-10-31 13:23:45 +0100

    Create VmdtLayout.java

---
+++ VmdtLayout.java
@@ -0,0 +1,28 @@
+package com.example.vmdtsncf;
+
+public class VmdtLayout extends ViewGroup {
+
+    public VmdtLayout(Context context) {
+        super(context, null);
+    }
+    
+    public VmdtLayout(Context context, AttributeSet attrs) {
+        super(context, attrs, 0);
+    }
+    
+    public VmdtLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+    
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+      
+      
+    }
+    
+    
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+    
+    }
+}
