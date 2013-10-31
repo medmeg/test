9703e7090abb6ac6a33374ce6e9fbc1498da301a
medmeg <mehdi.megzari@mysportconnect.net>
2013-10-31 13:31:46 +0100

    Update VmdtLayout.java

--- VmdtLayout.java
+++ VmdtLayout.java
@@ -1,5 +1,3 @@
-package com.example.vmdtsncf;
-
 public class VmdtLayout extends ViewGroup {
 
     public VmdtLayout(Context context) {
@@ -13,12 +11,18 @@ public class VmdtLayout extends ViewGroup {
     }
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int count = getChildCount();
+        for (int i = 0; i < count; i++) {
+            final View child = getChildAt(i);
         // chaque vue contenu dans le ViewGroup mesure ses propores dimensions
         // A partir de ces mesures on calcul la distribution relatives des icônes
-      
+        }
     }
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        for (int i = 0; i < count; i++) {
+            final View child = getChildAt(i);
         // Positionne cahque vue fils du ViewGroup dans le layout
+        }
     }
 }
